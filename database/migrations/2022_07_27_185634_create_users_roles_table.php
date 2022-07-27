a67ccab0d7e01110037e13b280384bb9d851d6be
M-Arslan <mughalarslan996@gmail.com>
2022-07-28 00:32:32 +0500

    testing

--- database/migrations/2022_07_27_185634_create_users_roles_table.php
+++ database/migrations/2022_07_27_185634_create_users_roles_table.php
@@ -25,9 +25,11 @@ return new class extends Migration
      * Reverse the migrations.
      *
      * @return void
+     * 
      */
     public function down()
     {
+
         Schema::dropIfExists('role_user');
     }
 };
